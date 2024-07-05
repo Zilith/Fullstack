20f240d7daeb639c880c53cc6aa68622881739a4
Diego Zapata <diegozapata063@gmail.com>
2024-07-05 13:14:47 -0500

    feat: exercise 1.7 done
    complete exercise 1.7 of the course

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,7 +8,7 @@ const Button = ({ handleClick, text }) => (
 
 const Statistics = ({ text, value }) => (
   <div>
-      {text} {value}
+    {text} {value}
   </div>
 );
 
@@ -17,19 +17,36 @@ function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+
+  const handleGood = () => {
+    setGood(good + 1);
+    setAll(all + 1);
+  };
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+    setAll(all + 1);
+  };
+  const handleBad = () => {
+    setBad(bad + 1);
+    setAll(all + 1);
+  };
 
   return (
     <>
       <Title text="give feedback" />
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Button handleClick={handleGood} text="good" />
+      <Button handleClick={handleNeutral} text="neutral" />
+      <Button handleClick={handleBad} text="bad" />
       <Title text="statistics" />
       <Statistics text="good" value={good} />
       <Statistics text="neutral" value={neutral} />
       <Statistics text="bad" value={bad} />
+      <Statistics text="all" value={all} />
+      <Statistics text="avarege" value={(good - bad) / all} />
+      <Statistics text="positive" value={[(good / all) * 100, "%"]} />
     </>
-  )
+  );
 }
 
 export default App;
