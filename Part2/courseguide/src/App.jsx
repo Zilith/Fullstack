38bc1b0a82fc3163ac2ac3708c444524595efc07
Diego Zapata <diegozapata063@gmail.com>
2024-07-21 15:46:46 -0500

    feat: add part2 examples

---
+++ Part2/courseguide/src/App.jsx
@@ -0,0 +1,18 @@
+const Note = ({ note }) => {
+  return <li>{note.content}</li>;
+};
+
+const App = ({ notes }) => {
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((n) => (
+          <Note key={n.id} note={n} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
