5d48c47aaff1b664a9835db711568bb1631195d6
Diego Zapata <diegozapata063@gmail.com>
2024-08-06 22:01:43 -0500

    feat: the notes are POST to the server

--- Part2/courseguide/src/App.jsx
+++ Part2/courseguide/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
     });
   };
 
-  useEffect(hook, [])
+  useEffect(hook, []);
 
   const handleNoteChange = (event) => {
     console.log(event.target.value);
@@ -27,11 +27,13 @@ const App = () => {
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-      id: notes.lenght + 1,
     };
 
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
+    axios.post("http://localhost:3001/notes", noteObject).then((response) => {
+      console.log(response);
+      setNotes(notes.concat(response.data));
+      setNewNote("");
+    });
   };
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
