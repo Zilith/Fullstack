6f007b75c6a416fc7f46a1701022875aa13e67c1
Diego Zapata <99824449+Zilith@users.noreply.github.com>
2025-02-28 12:29:05 -0500

    feat(index.js): add CORS support
    Enable Cross-Origin Resourse Sharing (CORS) in the backend

--- part3/courseguide/index.js
+++ part3/courseguide/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 let notes = [
   {
@@ -27,6 +28,7 @@ const requestLogger = (req, res, next) => {
   next();
 };
 
+app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 
@@ -86,7 +88,7 @@ app.delete("/api/notes/:id", (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
 });
