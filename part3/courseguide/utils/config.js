fded81983158cc234b5ffee20078c0745257b136
Diego Zapata <99824449+Zilith@users.noreply.github.com>
2025-04-24 17:49:39 -0500

    fix: resolve issue with PORT configuration

--- part3/courseguide/utils/config.js
+++ part3/courseguide/utils/config.js
@@ -1,6 +1,6 @@
-require('dotevn').config()
+require('dotenv').config()
 
-const PORT = process.env.port
+const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI
 
 module.exports = { PORT, MONGODB_URI }
