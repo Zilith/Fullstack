eb1b9e8dea964f68a27884ac70ce9eced2db5a9c
Diego Zapata <99824449+Zilith@users.noreply.github.com>
2025-04-24 20:27:24 -0500

    feat(mongo.js): use script for MongoDB data injection

--- part3/courseguide/mongo.js
+++ part3/courseguide/mongo.js
@@ -1,36 +1,43 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log("Give password as argument");
-  process.exit(1);
+  console.log('Give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
-const url = `mongodb+srv://Fullstack:${password}@cluster0.owsfs.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`;
+const password = process.argv[2]
+const url = `mongodb+srv://Fullstack:${password}@cluster0.owsfs.mongodb.net/testNoteApp?retryWrites=true&w=majority&appName=Cluster0`
 
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
-});
+})
 
-const Note = mongoose.model("Note", noteSchema);
+const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-  content: "mongoose makes things easy",
+  content: 'mongoose makes things easy',
   important: true,
-});
+})
 
-// note.save().then(result => {
-//     console.log('note saved!')
-//     mongoose.connection.close()
-// })
+note.save().then((result) => {
+  console.log('note saved!')
+})
+
+const note2 = new Note({
+  content: 'HTML is easy',
+})
+
+note2.save().then((result) => {
+  console.log('note saved!')
+})
 
 Note.find({}).then((result) => {
   result.forEach((note) => {
-    console.log(note);
-  });
-  mongoose.connection.close();
-});
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
